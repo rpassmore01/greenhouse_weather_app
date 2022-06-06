9089f9e0f0c37355c2bedc7cd23e205679901206
MetallicGoat <the.great.metallic.goat@gmail.com>
2022-06-05 20:41:27 -0400

    Add refresh button

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,13 @@ void main() {
 bool isLoaded = false;
 var _weatherDataJson = [];
 
-class Graph extends StatelessWidget{
+class Graph extends StatelessWidget {
   final List<charts.Series<dynamic, DateTime>> seriesList;
   final bool animate;
   final String graphMode;
 
-  const Graph(this.seriesList, {required this.animate, required this.graphMode});
+  const Graph(this.seriesList,
+      {required this.animate, required this.graphMode});
 
   @override
   Widget build(BuildContext context) {
@@ -28,17 +29,14 @@ class Graph extends StatelessWidget{
         charts.ChartTitle('time',
             behaviorPosition: charts.BehaviorPosition.bottom,
             titleOutsideJustification:
-            charts.OutsideJustification.middleDrawArea
-        ),
+                charts.OutsideJustification.middleDrawArea),
         charts.ChartTitle(graphMode,
             behaviorPosition: charts.BehaviorPosition.start,
             titleOutsideJustification:
-            charts.OutsideJustification.middleDrawArea
-        ),
+                charts.OutsideJustification.middleDrawArea),
       ],
     );
   }
-
 }
 
 class GenericTimeSeries {
@@ -48,12 +46,12 @@ class GenericTimeSeries {
   GenericTimeSeries(this.time, this.dataPoint);
 }
 
-class GraphWithDropdown extends StatefulWidget{
+class GraphWithDropdown extends StatefulWidget {
   @override
   _GraphWithDropdown createState() => _GraphWithDropdown();
 }
 
-class _GraphWithDropdown extends State{
+class _GraphWithDropdown extends State {
   String selectedDate = "day";
   String selectedMode = "temperature";
 
@@ -61,41 +59,43 @@ class _GraphWithDropdown extends State{
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
 
-    return Column(
-      children: [
-        DropdownButton(
-          value: selectedMode,
-          items: modeSelectorItems,
-          onChanged: (String? newValue){
-            setState(() {
-              selectedMode = newValue!;
-              _createSampleData();
-            });
-          },
+    return Column(children: [
+      DropdownButton(
+        value: selectedMode,
+        items: modeSelectorItems,
+        onChanged: (String? newValue) {
+          setState(() {
+            selectedMode = newValue!;
+            _createSampleData();
+          });
+        },
+      ),
+      SizedBox(
+        height: size.height * .35, // Set size of graph here (35% of screen)
+        child: Graph(
+          _createSampleData(),
+          animate: false,
+          graphMode: selectedMode,
         ),
-        SizedBox(
-          height: size.height * .45, // Set size of graph here (35% of screen)
-          child: Graph(_createSampleData(), animate: false, graphMode: selectedMode,),
-        ),
-        DropdownButton(
+      ),
+      DropdownButton(
         value: selectedDate,
         items: dateSelectorItems,
-      onChanged: (String? newValue){
+        onChanged: (String? newValue) {
           setState(() {
             selectedDate = newValue!;
             _createSampleData();
           });
-      },
-    ),
-      ]
-    );
+        },
+      ),
+    ]);
   }
 
-   List<charts.Series<GenericTimeSeries, DateTime>> _createSampleData() {
+  List<charts.Series<GenericTimeSeries, DateTime>> _createSampleData() {
     final List<GenericTimeSeries> data = [];
     Duration duration = const Duration(days: 0);
 
-    switch(selectedDate){
+    switch (selectedDate) {
       case "day":
         duration = const Duration(days: 1);
         break;
@@ -110,12 +110,14 @@ class _GraphWithDropdown extends State{
         break;
     }
 
-    for(var i = 0; i < _weatherDataJson.length; i++){
+    for (var i = 0; i < _weatherDataJson.length; i++) {
       final today = DateTime.now();
       DateTime currDateTime = DateTime.parse(_weatherDataJson[i]['created_on']);
 
-      if(today.subtract(duration).isBefore(currDateTime) || duration.inDays == 0){
-        data.add(GenericTimeSeries(currDateTime, _weatherDataJson[i][selectedMode]));
+      if (today.subtract(duration).isBefore(currDateTime) ||
+          duration.inDays == 0) {
+        data.add(
+            GenericTimeSeries(currDateTime, _weatherDataJson[i][selectedMode]));
       }
     }
 
@@ -132,11 +134,26 @@ class _GraphWithDropdown extends State{
 
   List<DropdownMenuItem<String>> get dateSelectorItems {
     List<DropdownMenuItem<String>> menuItems = [
-      const DropdownMenuItem(value: "day",child: Text("1 day"),),
-      const DropdownMenuItem(value: "week",child: Text("1 week"),),
-      const DropdownMenuItem(value: "month",child: Text("1 month"),),
-      const DropdownMenuItem(value: "year",child: Text("1 year"),),
-      const DropdownMenuItem(value: "all",child: Text("All time"),),
+      const DropdownMenuItem(
+        value: "day",
+        child: Text("1 day"),
+      ),
+      const DropdownMenuItem(
+        value: "week",
+        child: Text("1 week"),
+      ),
+      const DropdownMenuItem(
+        value: "month",
+        child: Text("1 month"),
+      ),
+      const DropdownMenuItem(
+        value: "year",
+        child: Text("1 year"),
+      ),
+      const DropdownMenuItem(
+        value: "all",
+        child: Text("All time"),
+      ),
     ];
 
     return menuItems;
@@ -144,12 +161,36 @@ class _GraphWithDropdown extends State{
 
   List<DropdownMenuItem<String>> get modeSelectorItems {
     List<DropdownMenuItem<String>> menuItems = [
-      const DropdownMenuItem(value: "temperature",child: Text("temperature"),),
-      const DropdownMenuItem(value: "humidity",child: Text("humidity"),),
+      const DropdownMenuItem(
+        value: "temperature",
+        child: Text("temperature"),
+      ),
+      const DropdownMenuItem(
+        value: "humidity",
+        child: Text("humidity"),
+      ),
     ];
 
     return menuItems;
   }
+}
+
+Future<List> loadWeatherData() async{
+  const url = "https://greenhouse.russellpassmore.me";
+
+  try {
+    final weatherData = await get(Uri.parse(url));
+    final jsonWeatherData = jsonDecode(weatherData.body) as Map;
+    List<dynamic> data = jsonWeatherData["data"];
+
+    print(data);
+
+    return data;
+
+  } catch (err) {
+    print("Error: ${err}");
+  }
+  return [];
 }
 
 class MyApp extends StatefulWidget {
@@ -158,24 +199,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final url = "https://greenhouse.russellpassmore.me";
 
-  void fetchWeatherData() async {
-    try {
-      final weatherData = await get(Uri.parse(url));
-      final jsonWeatherData = jsonDecode(weatherData.body) as Map;
-      List<dynamic> data = jsonWeatherData["data"];
-
-      print(data);
-
+  void fetchWeatherData() {
+    
+    loadWeatherData().then((value) {
       setState(() {
-        _weatherDataJson = data;
-        isLoaded = true;
+        if(value.isEmpty) {
+          isLoaded = false;
+        }else{
+          _weatherDataJson = value;
+          isLoaded = true;
+        }
       });
-
-    } catch (err) {
-      print("Error: ${err}");
-    }
+    });
   }
 
   @override
@@ -188,21 +224,20 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     //final double width = MediaQuery.of(context).size;
     return MaterialApp(
-        home: Home(),
-        title: "Greenhouse Weather",
+      home: Home(),
+      title: "Greenhouse Weather",
     );
   }
 }
 
-class Home extends StatefulWidget{
-@override
-_Home createState() => _Home();
+class Home extends StatefulWidget {
+  @override
+  _Home createState() => _Home();
 }
 
 class _Home extends State {
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
         appBar: AppBar(
@@ -211,7 +246,6 @@ class _Home extends State {
         body: Column(
           children: <Widget>[
             SizedBox(height: size.width * .03),
-
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -239,51 +273,74 @@ class _Home extends State {
               ],
             ),
             SizedBox(height: size.width * .05),
+
+            // Graph
             GraphWithDropdown(),
+
+            // Refresh Button
+            TextButton(
+              //TODO should we check if we are already refreshing before
+                onPressed: () {
+                  // Reset to defaults, and set state to show loading
+                  isLoaded = false;
+                  _weatherDataJson = [];
+                  setState(() {});
+
+                  loadWeatherData().then((value) {
+                    setState(() {
+                      if(value.isEmpty) {
+                        isLoaded = false;
+                      }else{
+                        _weatherDataJson = value;
+                        isLoaded = true;
+                      }
+                    });
+                  });
+                },
+                child: const Text('Refresh ⟳'))
           ],
-        )
-    );
+        ));
   }
 }
 
-List<Text> getInfo(String infoId, String prefix, String suffix){
-  if(!isLoaded) {
-    return [const Text("Loading...", textAlign: TextAlign.center,
-        style: TextStyle(
-            fontSize: 19,
-            fontWeight: FontWeight.w500))];
+List<Text> getInfo(String infoId, String prefix, String suffix) {
+  if (!isLoaded) {
+    return [
+      const Text("Loading...",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 19, fontWeight: FontWeight.w500))
+    ];
   }
 
-  if(infoId == "created_on"){
+  if (infoId == "created_on") {
     return [
-      Text(prefix, style: const TextStyle(
-        fontSize: 19,
-        fontWeight: FontWeight.w400)),
-      Text("${formatDate(_weatherDataJson[_weatherDataJson.length - 1][infoId])}$suffix", textAlign: TextAlign.center,
-        style: const TextStyle(
-            fontSize: 19,
-            fontWeight: FontWeight.w500))
+      Text(prefix,
+          style: const TextStyle(fontSize: 19, fontWeight: FontWeight.w400)),
+      Text(
+          "${formatDate(_weatherDataJson[_weatherDataJson.length - 1][infoId])}$suffix",
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 19, fontWeight: FontWeight.w500))
     ];
   }
 
   return [
-    Text(prefix, style: const TextStyle(
-      fontSize: 19,
-      fontWeight: FontWeight.w400)),
-    Text("${_weatherDataJson[_weatherDataJson.length - 1][infoId].toString()}$suffix", textAlign: TextAlign.center,
-      style: const TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.w500))
+    Text(prefix,
+        style: const TextStyle(fontSize: 19, fontWeight: FontWeight.w400)),
+    Text(
+        "${_weatherDataJson[_weatherDataJson.length - 1][infoId].toString()}$suffix",
+        textAlign: TextAlign.center,
+        style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w500))
   ];
 }
 
 class InfoBox extends StatelessWidget {
-  const InfoBox({Key? key,
-    required this.color,
-    required this.width,
-    required this.height,
-    required this.data
-  }) : super(key: key);
+  const InfoBox(
+      {Key? key,
+      required this.color,
+      required this.width,
+      required this.height,
+      required this.data})
+      : super(key: key);
 
   final Color color;
   final double width;
@@ -294,18 +351,18 @@ class InfoBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Flexible(
       child: FractionallySizedBox(
-          widthFactor: width,
-          child: Container(
-            decoration: BoxDecoration(
+        widthFactor: width,
+        child: Container(
+          decoration: BoxDecoration(
               color: color,
               borderRadius: const BorderRadius.all(Radius.circular(10))),
-            height: height,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: data,
-            ),
-            ),
+          height: height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: data,
           ),
+        ),
+      ),
     );
   }
 }
